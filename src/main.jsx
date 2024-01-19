534649c31721e0dafc04378719bc4499b4aeb364
Christer Miroy <chrstrzm.mry@gmail.com>
2024-01-20 06:37:56 +0800

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { HashRouter } from 'react-router-dom'
+// import { HashRouter } from 'react-router-dom'
 // import { RouterProvider } from 'react-router-dom'
 // import router from './router.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <RouterProvider router={router} /> */}
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>,
 )
